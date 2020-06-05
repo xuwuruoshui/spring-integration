d90fe66eb14552c092daf57eaf787fcc20dbe834
xuwuruoshui <1085252985@qq.com>
2020-06-05 14:04:52 +0800

    del some extra stuff

---
+++ social-login/src/main/java/top/xuwuruoshui/sociallogin/SocialLoginApplication.java
@@ -0,0 +1,13 @@
+package top.xuwuruoshui.sociallogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialLoginApplication.class, args);
+    }
+
+}
